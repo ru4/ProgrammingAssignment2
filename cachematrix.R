103bfb8d1fea37259660ec41fcfde23f4898a9b9
ru4 <r_u4@hotmail.com>
2015-09-11 12:29:44 +0300

    Update the cachematrix.R file and create makeCacheMatrix(), cacheSolve() functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function calculate the inverse of a matrix and cache it for further use.
 
-## Write a short comment describing this function
+## create the spacial Matrix with setter and getter for Matrix data and setter, getter for inverse of Matrix data
+## setinv() caches the Matrix in "m" variable in its parent env. 
 
 makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL
+	set <- function(y) {
+			x <<- y
+			m <<- NULL
+	}
+	get <- function() x
+	setinv <- function(inv) m <<- inv
+	getinv <- function() m
+	list(set = set, get = get,
+		 setinv = setinv,
+		 getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## return the inverse of the spacial matrix from cache if its already there or calculates and cache it via setinv() then return it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		
+		m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
